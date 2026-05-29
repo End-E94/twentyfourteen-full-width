fcc84e8b30acf164b6380852670bea7ec0d90b7e
End-E94 <radojevandrija@gmail.com>
2026-05-29 23:03:18 +0200

    Bump version to 1.0.1 and update compatibility

--- full-width-for-twenty-fourteen.php
+++ full-width-for-twenty-fourteen.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: Full Width for Twenty Fourteen
- * Version: 1.0.0
+ * Version: 1.0.1
  * Description: A simple user friendly plugin which converts the Twenty Fourteen theme to full width
  * Author: Andrija Radojev
  * Author URI: https://radojev.com/
  * Requires at least: 4.0
- * Tested up to: 6.4.1
+ * Tested up to: 7.0
  *
  * Text Domain: full-width-for-twenty-fourteen
  * Domain Path: /lang/
@@ -36,7 +36,7 @@ require_once 'includes/lib/class-full-width-for-twenty-fourteen-taxonomy.php';
  * @return object Full_Width_for_Twenty_Fourteen
  */
 function fwftf_full_width_for_twenty_fourteen() {
-	$instance = Full_Width_for_Twenty_Fourteen::instance( __FILE__, '1.0.0' );
+	$instance = Full_Width_for_Twenty_Fourteen::instance( __FILE__, '1.0.1' );
 
 	if ( is_null( $instance->settings ) ) {
 		$instance->settings = Full_Width_for_Twenty_Fourteen_Settings::instance( $instance );
